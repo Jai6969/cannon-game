0fbbb73ea8210c203093d855e12a72a20b6670c2
Jai6969 <90469491+Jai6969@users.noreply.github.com>
2021-11-05 17:52:23 +0000

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,21 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.canon_image=loadImage("assets/canon.png");
+    this.canon_base=loadImage("assets/cannonBase.png");
+
+  }
+  display(){
+    push();
+    translate(this.x, this.y)
+    imageMode(CENTER);
+    image(this.canon_image,0,0,this.width,this.height);
+    pop();
+
+    image(this.canon_base,70,20,200,200)
+  }
+}
